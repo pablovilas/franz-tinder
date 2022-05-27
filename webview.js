672f0057447dee2ff38730dcb5a6ed4786cf0efa
Pablo Vilas <pablovilas89@gmail.com>
2022-05-27 11:44:01 -0300

    Add message count support

--- webview.js
+++ webview.js
@@ -1,8 +1,13 @@
+'use strict';
+
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    // set Franz badge
-    Franz.setBadge(0);
+    let newMessages = document.querySelectorAll('.messageList > .messageListItem.messageListItem--isNew');
+    if (newMessages) {
+        let count = newMessages.length;
+        Franz.setBadge(count);
+    }
   };
-  // check for new messages every second and update Franz badge
   Franz.loop(getMessages);
+  Franz.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
